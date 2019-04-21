1a614afa6f18bf135e1002fbf556c57cead61474
S.F <you@example.com>
2019-04-21 13:53:03 +0900

    update

--- index.js
+++ index.js
@@ -2216,6 +2216,14 @@ var mwasmParser = {
   parse:       peg$parse
 };
 
+function error(message,token){
+  if(token){
+    throw new Error(`Error: line:${token.start.line} column:${token.start.column} :${message}:`)
+  } else {
+    throw new Error(message);    
+  }
+}
+
 var index = async () => {
 
   try {
@@ -2264,8 +2272,116 @@ var index = async () => {
 
 
     const wabt_ = wabt();
-  
-    let tokens = mwasmParser.parse(await fs.promises.readFile(args.input,'utf-8'));
+
+    class Context {
+      constructor(parser){
+        this.parser = parser;
+        this.includeFileTree = {parent:null,childs:{}};
+        this.path = path;
+        this.require = require;
+        this.pathStack = [];
+      }
+
+      readSourceFile(srcPath){
+        srcPath = path.normalize(srcPath);
+        let src = fs.readFileSync(srcPath,'utf-8');
+        return src;
+      }
+
+      checkIncludeLoop(baseName){
+        let current = this.includeFileTree;
+        function check(b){
+          if(current.parent && current.parent[b]){
+            return false;
+          } else if(current.parent){
+            current = current.parent;
+            return check(b);
+          }
+          return true;
+        }
+        return check(baseName);
+      }
+
+
+      preprocess(srcPath,srcToken){
+
+        const baseName = path.basename(srcPath);
+
+        if(!this.checkIncludeLoop(baseName)){
+          error(`include Loop Detected:${baseName}`,srcToken);
+        }
+
+        if(!(baseName in this.includeFileTree.childs)){
+          const inode =  {parent:this.includeFileTree,childs:{}};
+          inode.parent.childs[baseName] = inode;
+          this.includeFileTree = inode;
+        } else {
+          const inode = this.includeFileTree.childs[baseName];
+          this.includeFileTree = inode;
+        }
+
+        let srcStr = this.readSourceFile(srcPath);
+
+        let tokens = this.parser.parse(srcStr);
+
+        const preprocessed = [];
+        
+        for(const token of tokens){
+          switch(token.type){
+            case 'LineTerminator':
+              preprocessed.push('\n');
+              break;
+            case 'Identifier':
+              preprocessed.push(token.name);
+              break;
+            case 'Separator':
+              preprocessed.push(token.kind);
+              break;
+            case 'StringLiteral':
+              preprocessed.push(token.value);
+              break;
+            case 'Comment':
+              preprocessed.push(token.value);
+              break;
+            case 'CodeExpression':
+              preprocessed.push(this.evalExpression(token.code));
+              break;
+            case 'CodeWithoutReturnValue':
+              this.eval(token.code);
+              break;
+            case 'Code':
+              preprocessed.push(this.eval(token.code));
+              break;
+            case 'SourceInclude':
+              console.log(token.pathExpression);
+              let src = this.evalExpression(token.pathExpression);
+              this.preprocess(src,token);
+              break;
+            case 'PropertyGet':
+              break;
+            default:
+          }
+        }
+
+        this.includeFileTree = this.includeFileTree.parent;
+        return preprocessed.join(' ');
+      }
+
+
+      eval(code){
+        let func = new Function('$',code);
+        return func.bind(this)(this);
+      }
+
+      evalExpression(code){
+        return this.eval('return ' + code);
+      }
+    }
+    
+    const context = new Context(mwasmParser);
+    console.log(args);
+    context.preprocess(args.input);
+    // let tokens = parser.parse(await fs.promises.readFile(args.input,'utf-8'));
     console.log(tokens);
    
     // let preprocessedSource = tokens.join(' ');
