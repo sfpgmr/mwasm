e50f789e736838360a138b49236c94048a745379
sfpgmr <sfpgmr@example.com>
2019-06-05 06:27:15 +0900

    update

---
+++ examples/psg-emulator/test.js
@@ -0,0 +1,34 @@
+"use strict";
+const fs = require('fs');
+function getInstance(obj, imports = {}) {
+  const bin = new WebAssembly.Module(obj);
+  const inst = new WebAssembly.Instance(bin, imports);
+  return inst;
+}
+
+(async () => {
+  const psg = getInstance(await fs.promises.readFile('./em2149.wasm')).exports;
+  psg.init(1790000, 22050);
+  psg.reset();
+  psg.setRate(44100);
+  psg.setQuality(1);
+  psg.setVolumeMode(1);
+
+  psg.writeReg(0, 0x5d);
+  psg.writeReg(1, 0xd);
+  psg.writeReg(2, 0x5d);
+  psg.writeReg(3, 0x1);
+  psg.writeReg(4, 0x5d);
+  psg.writeReg(5, 0x2);
+  psg.writeReg(7, 0b111110);
+  psg.writeReg(6, 0x10);
+  psg.writeReg(13, 0b0001);
+  psg.writeReg(11, 100);
+  psg.writeReg(8, 0b01000);
+
+  for (let i = 0; i < 256; ++i) {
+    console.log(psg.calc());
+  }
+})();
+
+
